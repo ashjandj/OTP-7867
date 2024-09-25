5ab000871e122284613b2b65ac75cadd491eb646
ashjandj <aiswarya.babu@jobinandjismi.com>
2024-09-25 15:10:32 +0530

    Update jj_cs_blood_donar_otp_7867.js

--- OTP-7528/src/FileCabinet/SuiteScripts/JobinAndJismi2/OTP-7528/jj_cs_blood_donar_otp_7867.js
+++ OTP-7528/src/FileCabinet/SuiteScripts/JobinAndJismi2/OTP-7528/jj_cs_blood_donar_otp_7867.js
@@ -32,20 +32,6 @@ define(['N/format', 'N/record', 'N/search'],
      * @param{search} search
      */
     function (format, record, search) {
-
-        /**
-         * Function to be executed after page is initialized.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
-         *
-         * @since 2015.2
-         */
-        function pageInit(scriptContext) {
-
-        }
-
         /**
          * Function to be executed when field is changed.
          *
@@ -71,124 +57,6 @@ define(['N/format', 'N/record', 'N/search'],
                 }
             }
         }
-
-
-        /**
-         * Function to be executed when field is slaved.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         * @param {string} scriptContext.fieldId - Field name
-         *
-         * @since 2015.2
-         */
-        function postSourcing(scriptContext) {
-
-        }
-
-        /**
-         * Function to be executed after sublist is inserted, removed, or edited.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @since 2015.2
-         */
-        function sublistChanged(scriptContext) {
-
-        }
-
-        /**
-         * Function to be executed after line is selected.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @since 2015.2
-         */
-        function lineInit(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when field is changed.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         * @param {string} scriptContext.fieldId - Field name
-         * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-         * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-         *
-         * @returns {boolean} Return true if field is valid
-         *
-         * @since 2015.2
-         */
-        function validateField(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when sublist line is committed.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateLine(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when sublist line is inserted.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateInsert(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when record is deleted.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateDelete(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when record is saved.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @returns {boolean} Return true if record is valid
-         *
-         * @since 2015.2
-         */
-        function saveRecord(scriptContext) {
-
-        }
-
         /**
             * Searches for eligible blood donors based on the provided blood group and last donation date.
             * Populates the sublist on the form with matching donor details (First Name, Last Name, Phone Number).
@@ -281,7 +149,7 @@ define(['N/format', 'N/record', 'N/search'],
         */
         function removeSublistLine(formRecord) {
             try {
-                var lineCount = formRecord.getLineCount('custpage_jj_blood_sublist');
+                let lineCount = formRecord.getLineCount('custpage_jj_blood_sublist');
                 for (let i = 0; i < lineCount; i++) {
 
 
@@ -296,16 +164,8 @@ define(['N/format', 'N/record', 'N/search'],
             }
         }
         return {
-            // pageInit: pageInit,
-            fieldChanged: fieldChanged,
-            // postSourcing: postSourcing,
-            // sublistChanged: sublistChanged,
-            // lineInit: lineInit,
-            // validateField: validateField,
-            // validateLine: validateLine,
-            // validateInsert: validateInsert,
-            // validateDelete: validateDelete,
-            // saveRecord: saveRecord
+            fieldChanged: fieldChanged
+
         };
 
     });
